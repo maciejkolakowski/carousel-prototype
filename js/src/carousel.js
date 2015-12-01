ee6e06ce93a9c04bcf8eda2b783faeb51afa421b
Maciej Kołakowski <maciej.kolakowski1@gmail.com>
2015-12-01 23:48:59 +0100

    prettify

--- js/src/carousel.js
+++ js/src/carousel.js
@@ -1,7 +1,8 @@
 'use strict';
-function carousel(selector, options){
+
+function carousel(selector, options) {
     var options = options;
-    if(options === undefined) {
+    if (options === undefined) {
         options = {
             slideDuration: 5000,
             speed: 700
@@ -20,9 +21,9 @@ function carousel(selector, options){
     this.init();
 }
 
-carousel.prototype.changeSlide = function(changeType){
-    
-    if(this.animated) {
+carousel.prototype.changeSlide = function(changeType) {
+
+    if (this.animated) {
         return;
     }
     this.animated = true;
@@ -30,52 +31,62 @@ carousel.prototype.changeSlide = function(changeType){
 
     var activeSlide;
     var transformedSpeed = this.defaults.speed / 1000;
-    
+
     activeSlide = this.slidesWrapper.querySelector('.' + this.activeClassName);
-    
-    if(!this.cssTransitions) {
-        TweenLite.to(activeSlide, transformedSpeed, {opacity: 0});
+
+    if (!this.cssTransitions) {
+        TweenLite.to(activeSlide, transformedSpeed, {
+            opacity: 0
+        });
     }
-    
+
     activeSlide.classList.remove(this.activeClassName);
 
-    switch(changeType) {
+    switch (changeType) {
 
         case 'next':
-                if(activeSlide.nextElementSibling) {
-                    activeSlide.nextElementSibling.classList.add(this.activeClassName);
-                    if(!this.cssTransitions) {
-                        TweenLite.to(activeSlide.nextElementSibling, transformedSpeed, {opacity:1});
-                    }
-                } else {
-                    this.slides[0].classList.add(this.activeClassName);
-                    if(!this.cssTransitions) {
-                        TweenLite.to(this.slides[0], transformedSpeed, {opacity:1});
-                    }
+            if (activeSlide.nextElementSibling) {
+                activeSlide.nextElementSibling.classList.add(this.activeClassName);
+                if (!this.cssTransitions) {
+                    TweenLite.to(activeSlide.nextElementSibling, transformedSpeed, {
+                        opacity: 1
+                    });
                 }
+            } else {
+                this.slides[0].classList.add(this.activeClassName);
+                if (!this.cssTransitions) {
+                    TweenLite.to(this.slides[0], transformedSpeed, {
+                        opacity: 1
+                    });
+                }
+            }
             break;
 
         case 'prev':
-                if(activeSlide.previousElementSibling) {
-                    activeSlide.previousElementSibling.classList.add(this.activeClassName);
-                    if(!this.cssTransitions) {
-                        TweenLite.to(activeSlide.previousElementSibling, transformedSpeed, {opacity:1});
-                    }
-                } else {
-                    this.slides[this.slides.length-1].classList.add(this.activeClassName);
-                    if(!this.cssTransitions) {
-                        TweenLite.to(this.slides[this.slides.length], transformedSpeed, {opacity:1});
-                    }
+            if (activeSlide.previousElementSibling) {
+                activeSlide.previousElementSibling.classList.add(this.activeClassName);
+                if (!this.cssTransitions) {
+                    TweenLite.to(activeSlide.previousElementSibling, transformedSpeed, {
+                        opacity: 1
+                    });
                 }
-                break;
+            } else {
+                this.slides[this.slides.length - 1].classList.add(this.activeClassName);
+                if (!this.cssTransitions) {
+                    TweenLite.to(this.slides[this.slides.length], transformedSpeed, {
+                        opacity: 1
+                    });
+                }
+            }
+            break;
 
     }
 
     this.initTimer();
-    
+
 };
 
-carousel.prototype.bindEvents = function(arrowLeft, arrowRight){
+carousel.prototype.bindEvents = function(arrowLeft, arrowRight) {
     var self = this;
     arrowLeft.addEventListener('click', function(e) {
         e.preventDefault();
@@ -87,44 +98,44 @@ carousel.prototype.bindEvents = function(arrowLeft, arrowRight){
     });
 }
 
-carousel.prototype.cssTransitionSupported = function(){
+carousel.prototype.cssTransitionSupported = function() {
     var elem = document.createElement('modernizr');
-    var props = ['transition','WebkitTransition','MozTransition','OTransition','msTransition'];
+    var props = ['transition', 'WebkitTransition', 'MozTransition', 'OTransition', 'msTransition'];
     for (var i in props) {
         var prop = props[i];
         var result = elem.style[prop] !== undefined ? prop : false;
-        if (result){
+        if (result) {
             this.cssTransitions = result;
             break;
-        } 
-    } 
+        }
+    }
 };
 
 carousel.prototype.clearTimer = function() {
-    if(this.carouselTimer) {
+    if (this.carouselTimer) {
         clearInterval(this.carouselTimer);
     }
 }
 
-carousel.prototype.initTimer = function(){
+carousel.prototype.initTimer = function() {
     var self = this;
     this.animated = false;
-    this.carouselTimer = setInterval(function(){
+    this.carouselTimer = setInterval(function() {
         self.changeSlide('next');
-    }, this.defaults.slideDuration);                    
+    }, this.defaults.slideDuration);
 };
 
-carousel.prototype.init = function(){
+carousel.prototype.init = function() {
 
     this.cssTransitionSupported();
-    
+
     this.activeClassName = this.cssTransitions ? 'active' : 'activate';
-    
+
     this.slides[0].classList.add(this.activeClassName);
-    
-    if(this.cssTransitions) {
+
+    if (this.cssTransitions) {
         var transitionName;
-        switch(this.cssTransitions) {
+        switch (this.cssTransitions) {
             case 'transition':
                 transitionName = 'transition';
                 break;
@@ -141,7 +152,7 @@ carousel.prototype.init = function(){
                 transitionName = '-ms-transition';
                 break;
         }
-        for(var i = 0; i < this.slides.length; i++) {
+        for (var i = 0; i < this.slides.length; i++) {
             this.slides[i].setAttribute('style', transitionName + ': ' + this.defaults.speed + 'ms opacity ease-in-out');
         }
     } else {
@@ -153,19 +164,19 @@ carousel.prototype.init = function(){
     var arrowRight = document.createElement('a');
     navContainer.classList.add('nav-container');
 
-    arrowLeft.classList.add('arrow','arrow-left');
-    arrowRight.classList.add('arrow','arrow-right');
+    arrowLeft.classList.add('arrow', 'arrow-left');
+    arrowRight.classList.add('arrow', 'arrow-right');
 
     arrowLeft.setAttribute('href', '#');
     arrowRight.setAttribute('href', '#');
 
     arrowLeft.innerHTML = '<span class="anchor-text">PREV</span>' +
-                          '<span class="anchor-background"></span>' +
-                          '<img src="images/arrow-left.png" alt="Previous slide">';
+        '<span class="anchor-background"></span>' +
+        '<img src="images/arrow-left.png" alt="Previous slide">';
 
     arrowRight.innerHTML = '<span class="anchor-text">NEXT</span>' +
-                           '<span class="anchor-background"></span>' +
-                           '<img src="images/arrow-right.png" alt="Next slide">';
+        '<span class="anchor-background"></span>' +
+        '<img src="images/arrow-right.png" alt="Next slide">';
 
     navContainer.appendChild(arrowLeft);
     navContainer.appendChild(arrowRight);
